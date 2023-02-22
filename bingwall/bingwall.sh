758e861609d3851578c5ea0c8b6476def410b0cc
wondersalmon <wonder.salmon@gmail.com>
2023-02-22 21:14:50 +0500

    improve script logic

--- bingwall/bingwall.sh
+++ bingwall/bingwall.sh
@@ -8,11 +8,11 @@ if [ ! -d "$path" ]; then
     mkdir -p "$path"
 fi
 
-# Get the current date and time
-current_time=$(date +"%Y-%m-%d_%H-%M-%S")
-
 while true; do
-    if nc -zw1 bing.com 443; then
+    # Check internet connectivity
+    if ping -q -c 1 -W 1 google.com >/dev/null; then
+        # Get the current date and time
+        current_time=$(date +"%Y-%m-%d_%H-%M-%S")
         # Download the Bing image of the day and save it to the specified path
         image_url="http://www.bing.com/$(wget -q -O- https://binged.it/2ZButYc | sed -e 's/<[^>]*>//g' | cut -d / -f2 | cut -d \& -f1)"
         image_title=$(echo $image_url | sed 's/.*OHR\.\(.*\)_ROW.*/\1/')
@@ -25,13 +25,20 @@ while true; do
             wget -O "$image_name" "$image_url"
             # Set the downloaded image as the desktop background
             xfconf-query -c xfce4-desktop -p $monitor -s "$path/$image_name"
-            # Send a notification with the image title
-            notify-send "Bing Wallpaper" "New wallpaper has been set: $image_title"
+            # Check if the previous wallpaper was the same as the current one
+            previous_wallpaper=$(xfconf-query -c xfce4-desktop -p $monitor -v | cut -d/ -f 5)
+            if [ "$previous_wallpaper" != "$image_name" ]; then
+                # Send a notification with the image title
+                notify-send "Bing Wallpaper" "New wallpaper has been set: $image_title"
+            fi
         fi
         # Remove any images older than the last 10 images
         ls -t | grep -v / | tail -n +11 | xargs -I {} rm -- {}
-        sleep 10800 # Sleep for 3 hours
     else
+        echo "No internet connection. Retrying in 20 seconds."
         sleep 20
+        continue
     fi
+    # Sleep for 1 hour
+    sleep 3600
 done
